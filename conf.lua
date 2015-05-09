8debcca8fdfe7aaf582cbb4b89902742d1344e14
Sebastian Nesgaard Jensen <snje@dtu.dk>
2015-05-09 20:06:46 +0200

    Changed title

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
     t.version = "0.9.1"                -- The LÖVE version this game was made for (string)
     t.console = false                  -- Attach a console (boolean, Windows only)
 
-    t.window.title = "Banana Bongo"        -- The window title (string)
+    t.window.title = "Impa Fires Guns"        -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 800               -- The window width (number)
     t.window.height = 600              -- The window height (number)
